f9ce702794243cd3c9ec419871643218bc7f1be4
M. Ridho Akbar <129049803+Akbarbtg445@users.noreply.github.com>
2025-04-15 20:15:09 +0700

    Update structArray.cpp

--- structArray.cpp
+++ structArray.cpp
@@ -15,12 +15,25 @@ int main(){
     //membuat object struct mahasiswa
     mahasiswa mhs[2];
 
-    for (int n= 0;n <2;n++){
-    cout << "Datake-" << n+1 << endl;
+    cout << "Masukan NIM = ";
+    cin >> mhs.nim;
+    cin.ignore();
+    cout << "Masukan NAMA =";
+    getline (cin,mhs.nama);
+    cin >> mhs.nama;
+    cout << "Masukan ALAMAT DESA =";
+    cin >> mhs.alamat.desa;
+    cout << "Masukan ALAMAT KOTA =";
+    cin >> mhs.alamat.kota;
+
+    cout << endl;
+    cout << "Data Mahasiswa" << endl;
+
+    for(int n = 0;n<2;n++)
+    cout << "Data ke-" << n+1 << endl;
     cout << "NIM = "<< mhs[n].nim << endl;
     cout << "NAMA = "<< mhs[n].nama << endl;
     cout << "ALAMAT DESA ="<< mhs[n].alamat.desa << endl;
     cout << "ALAMAT KOTA ="<< mhs[n].alamat.kota << endl;
-    }
 
 }
