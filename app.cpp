86c7cd516ce51950b9380cfcf610747d7ef6c541
Akbar <akbarbtg445@gmail.com>
2026-04-06 11:04:21 +0700

    membuat perulangan

--- app.cpp
+++ app.cpp
@@ -9,9 +9,34 @@ int dataku [5] = {10,20,30,40,50};
 int datamu [5];
 
 //cara akses (membaca atau mengambil isi array)
-cout << "isi data kedua : " << dataku [i] << endl;
+cout << "isi data kedua : " << dataku [1] << endl;
 //mengisi array
-dataku [i] = 200;
-cout << "isi data kedua setelah diubah : " << dataku[i] <<endl;
+dataku [1] = 200;
+cout << "isi data kedua setelah diubah : " << dataku[1] <<endl;
+
+//mengisi data dengan perintah satu persatu 
+cout << "isikan data pertama:" ;
+cin >> datamu[0];
+cout << "isikan data kedua:" ;
+cin >> datamu[1];
+cout << "isikan data ketiga:" ;
+cin >> datamu[2];
+cout << "isikan data keempat:" ;
+cin >> datamu[3];
+cout << "isikan data kelima:" ;
+cin >> datamu[4];
+
+//mengisi data melalui perulangan
+for (int i = 0; i <= 4; i++)
+{
+    cout << "Data ke-" << (i+1) << ":";
+    cin >> datamu[i];
+
+}
+//menampilkan data dengan perulangan
+for (int i = 0; i <= 4; i++)
+{
+    cout << "data ke-" << (i+1) << ":" << datamu[i] << endl;
+}
 
 }
